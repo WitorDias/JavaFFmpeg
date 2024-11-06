67686c0be4d1d3bd93acdd5aa8a4d806486f3053
WitorDias <joao_witor_dias@hotmail.com>
2024-11-06 10:52:25 -0300

    fix: controle de excessoes adicionado

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         OperacoesFFmpeg executar = new OperacoesFFmpeg();
-        executar.converterImagens();
+        executar.converterImagemJpgParaPng();
     }
 }
--- src/OperacoesFFmpeg.java
+++ src/OperacoesFFmpeg.java
@@ -2,24 +2,35 @@ import java.io.File;
 import java.io.IOException;
 
 public class OperacoesFFmpeg {
-    public void converterImagens() {
+    public void converterImagemJpgParaPng() {
+
         String caminhoEntrada = System.getenv("caminhoEntrada");
         String caminhoSaida = System.getenv("caminhoSaida");
         File diretorioSaida = new File(caminhoSaida);
-        diretorioSaida.mkdir();
+
+        if(!diretorioSaida.exists()) {
+            diretorioSaida.mkdir();
+        }
+
         File[] arquivos = new File(caminhoEntrada).listFiles();
+
         if (arquivos != null) {
             for (File file : arquivos) {
                 if (file.isFile() && file.getName().endsWith(".jpg")) {
                     String arquivoEntrada = file.getAbsolutePath();
                     String arquivoSaida = caminhoSaida + "\\" + file.getName().replace(".jpg", ".png");
-                    String[] comandoFfmpeg = {"ffmpeg", "-i", arquivoEntrada, arquivoSaida};
-                    try {
-                        Process process = Runtime.getRuntime().exec(comandoFfmpeg);
-                        process.waitFor();
+                    File arquivoSaidaConcreto = new File(arquivoSaida);
+                    if (!arquivoSaidaConcreto.exists()) {
+                        String[] comandoFfmpeg = {"ffmpeg", "-i", arquivoEntrada, arquivoSaida};
+                        try {
+                            Process process = Runtime.getRuntime().exec(comandoFfmpeg);
+                            process.waitFor();
 
-                    } catch (IOException | InterruptedException e) {
-                        System.err.println("Erro ao converter imagem " + file.getName() + ": " + e.getMessage());
+                        } catch (IOException | InterruptedException e) {
+                            System.err.println("Erro ao converter imagem " + file.getName() + ": " + e.getMessage());
+                        }
+                    }else {
+                        System.out.println("Arquivo: "+ arquivoSaida + " já existe.");
                     }
                 }
             }
